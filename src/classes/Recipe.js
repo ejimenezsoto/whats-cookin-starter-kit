6c25740b0c4da1c506130d1c3826ff4e14c45169
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-12-06 13:36:20 -0800

    Complete Recipe Class, Complete Recipe-test

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -1,6 +1,38 @@
-class Recipe{
-    constructor(){
 
+// {
+//     "id": 595736,
+//     "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+//     "ingredients": [
+//       {
+//         "id": 20081,
+//         "quantity": {
+//           "amount": 1.5,
+//           "unit": "c"
+//         }.
+//       ]
+//     "instructions": [
+//       {
+//         "instruction": "Remove the pan from the oven and let sit for 10 minutes before removing onto a cooling rack.Top with ice cream and a drizzle of chocolate sauce.",
+//         "number": 6
+//       }
+//     ],
+//     "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+//     "tags": [ ]
+//   }
+
+
+
+
+
+
+class Recipe{
+    constructor(id,image,ingredients,instructions,name,tags){
+        this.id = id;
+        this.image = image;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+        this.name = name;
+        this.tags = tags;
     }
 }
 export default Recipe
