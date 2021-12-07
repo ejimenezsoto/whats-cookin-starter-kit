74b34c9032921635eeb7b2666d0ee4b5ae022f8c
Jacob Quintana <jacobquintaa@gmail.com>
2021-12-06 21:20:05 -0700

    Create cookbook class

--- src/classes/Cookbook.js
+++ src/classes/Cookbook.js
@@ -1,8 +1,26 @@
-class Cookbook {
-  constructor() {
 
-    // One class to get you started!
+class Cookbook {
+  constructor(recipes) {
+    this.recipes = recipes;
+    this.tags = [];
   }
+  filterByTags(tag) {
+    this.tags.push(tag);
+    //were pushing a string (tag) into an array 
+    this.tags.forEach(tag => {
+      const filterdRecipes = (recipes, tag) => {
+        console.log(this.recipes, tag);
+      }
+      return filterdRecipes
+    })
+    // we want to filter through that array
+    // return a new array of recipes that include tags from tags array
+  }
+
+  filterByNameAndIngredients() {
+
+  }
+  
 }
 
 export default Cookbook;
