3a77fbe13640efc1b656c2e2877e78ed498b62c0
Kai Kuller <kavakai@gmail.com>
2021-12-09 16:00:05 -0700

    Begin DOM manipulation

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,31 @@
 import './styles.css';
 import apiCalls from './apiCalls';
-// import Recipe from '.src/classes/Cookbook.js';
-// import Cookbook from '.src/classes/Cookbook.js';
+import Cookbook from './classes/Cookbook';
 import './images/cooking.png'
+import Recipe from './classes/Recipe';
+import recipeData from './data/recipes'
+    
+console.log(recipeData, "!!!!!!!!")
 
+// const recipes = recipeData.map(recipe => {
+//     return new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags)
+// });
 
-// showRecipe(recipe) {
-//     /*somthing*/.innerHTML += `
-//     <img
-//     <h3>${recipe.name}</h3>
-//     `
-// }
+const recipeRow = document.querySelector('.all-recipes-section')
+
+const cookBook = new Cookbook(recipes)
+
+window.onload = displayRecipes();
+
+const displayRecipes = () => {
+  const allRecipies = cookBook.recipies.forEach(recipe => {
+    recipeRow.innerHTML += `
+    <div >
+        <img src="${recipe.image}" alt="${recipe.name}">
+        <h3>${recipe.name}</h3>
+    </div>` 
+  });
+  return allRecipies;
+}
 
 console.log('Hello world');
