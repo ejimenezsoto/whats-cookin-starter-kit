8279e8011dc47b482695d75dc793c9a086119800
Kai Kuller <kavakai@gmail.com>
2021-12-10 14:13:24 -0700

    Comment out filterTags method

--- src/scripts.js
+++ src/scripts.js
@@ -14,6 +14,8 @@ const singleRecipeSection = document.querySelector('.single-recipe');
 const filterBox = document.querySelector('.side-nav');
 
 
+
+
 const cookBook = new Cookbook(recipeData)
 
 
@@ -34,18 +36,20 @@ const getTags = recipes.reduce((acc, recipe) => {
   return acc;
 }, []);
     
-const filterTags = () => {
-  const filteredTags = getTags.forEach(tag => {
-    console.log(tag, "TAG")
-    return filterBox.innerHTML += `
-        <div class="tag-container">    
-        <input type="checkbox" id='${tag}' name='${tag}'>
-            <label for='${tag}'>'${tag}'</label>
-        </div>
-        `
-  })
-  return filteredTags;
-};
+// const filterTags = () => {
+//   const filteredTags = getTags.forEach(tag => {
+
+//     return filterBox.innerHTML += `
+//         <div class="tag-container">    
+//         <input class="checkbox" type="checkbox" id='${tag}' name='tag'>
+//             <label for='${tag}'>'${tag}'</label>
+//         </div>
+//         `
+//   })
+//   return filteredTags;
+// };
+
+
 
 const displayRecipes = () => {
   const allRecipies = cookBook.recipes.forEach(recipe => {
@@ -90,3 +94,18 @@ window.addEventListener('load', function () {
   filterTags();
 })
 allRecipesSection.addEventListener('click', clickRecipe);
+const checkboxes = document.querySelectorAll('input[type=checkbox][name=tag]')
+
+let tagList = [];
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+        tagList = Array.from(checkboxes)
+            .filter(i => i.checked)
+            .map(i => i.value)
+        console.log(tagList);
+    })
+})
+console.log(checkboxes, '!!!!')
+// checkboxes.addEventListener('change', function() {
+//     console.log("click")
+// })
