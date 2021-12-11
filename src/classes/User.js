06c0b4417c60e3e3e41ea709da4242bffbd0b0a0
Kai Kuller <kavakai@gmail.com>
2021-12-11 14:57:56 -0700

    Create user class and test file

---
+++ src/classes/User.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(userData) {
+        this.name = userData.name;
+        this.id = userData.id;
+        this.pantry = userData.pantry;
+    }
+}
+
+export default User
---
+++ test/User-test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import User from '..src/classes/User'
+
+describe('User', () => {
+   
+    const user = new User(userData)
+
+    it()
+})
