645f881e016af378a8bd3f6caedf31f33b372db5
Jacob Quintana <jacobquintaa@gmail.com>
2021-12-11 21:05:07 -0700

    Create random user function

--- src/scripts.js
+++ src/scripts.js
@@ -5,52 +5,37 @@ import './images/cooking.png'
 import Recipe from './classes/Recipe';
 import recipeData from './data/recipes.js'
 import User from './classes/User';
+import usersData from './data/users';
 
-const user = new User('Sam', 1, []);
+
 
 const recipes = recipeData.map(recipe => {
     return new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags)
 });
 
+const users = usersData.map(user => {
+  return new User(user.name, user.id, user.pantry)
+})
+
+// sections 
 const allRecipesSection = document.querySelector('.all-recipes-section');
 const singleRecipeSection = document.querySelector('.single-recipe');
-const filterBox = document.querySelector('.side-nav');
 const filteredRecipesSection = document.querySelector('#filter-recipes-section');
-const clearButton = document.querySelector('.clear-button')
-const searchInput = document.querySelector('#userInput')
+const filterBox = document.querySelector('.side-nav');
+//buttons
+const favoriteRecipesButton = document.querySelector('favorites')
+const clearButton = document.querySelector('.clear-button');
+const searchInput = document.querySelector('#userInput');
+const allRecipesButton = document.querySelector('.recipies');
+
+//global variables 
 let tagList = [];
-
-searchInput.addEventListener('input', (e) => {
-  let input = e.target.value
-
-  if(input && input.trim().length > 0){
-    input = input.trim().toLowerCase()
-    allRecipesSection.innerHTML = ''
-
-    allRecipesSection.innerHTML = cookBook.filterByKeyWord(input).reduce((acc,recipe) => {
-      acc += `
-        <div class='recipe'>
-      <img class="recipe-image" src="${recipe.image}" id='${recipe.id}' alt="${recipe.name}">
-      <h5>${recipe.name}</h5>
-      </div> 
-      `
-      
-    return acc
-  }, '')
-
-  } else {
-    console.log('Input is invalid')
-  }
-})
-
-clearButton.addEventListener("click", () => {
-  searchInput.value = ''
-  displayRecipes()
-})
-
+let isClicked = false;
+var currentUser;
+let filter;
 const cookBook = new Cookbook(recipes)
 
-
+//reusable functions 
 const hide = (element) => {
   element.classList.add('hidden')
 }
@@ -59,14 +44,37 @@ const show = (element) => {
   element.classList.remove('hidden')
 }
 
-const getTags = recipes.reduce((acc, recipe) => {
-  recipe.tags.forEach(tag => {
-    if (!acc.includes(tag)) {
-      acc.push(tag);
-    }
-  });
-  return acc;
-}, []);
+searchInput.addEventListener('input', (e) => {
+  searchRecipes(e)
+})
+
+const searchRecipes = (e) => {
+  let input = e.target.value
+
+  if (input && input.trim().length > 0) {
+    input = input.trim().toLowerCase()
+
+    allRecipesSection.innerHTML = filter.filterByKeyWord(input).reduce((acc, recipe) => {
+      acc += `
+        <div class='recipe'>
+      <img class="recipe-image" src="${recipe.image}" id='${recipe.id}' alt="${recipe.name}">
+      <h5>${recipe.name}</h5>
+      </div> 
+      `
+
+      return acc
+    }, '')
+
+  } else {
+    console.log('Input is invalid')
+  }
+}
+
+clearButton.addEventListener("click", () => {
+  searchInput.value = ''
+  displayRecipes()
+})
+
     
 const displayRecipes = () => {
   const allRecipies = cookBook.recipes.forEach(recipe => {
@@ -79,8 +87,7 @@ const displayRecipes = () => {
   return allRecipies;
 }
 
-const clickRecipe = (event) => {
-    
+const clickRecipe = (event) => {  
   hide(allRecipesSection)
   show(singleRecipeSection)
   const findRecipeId =  recipes.find( ( { id} ) => id == event.target.id)
@@ -102,49 +109,17 @@ const clickRecipe = (event) => {
     `
 }
 
+const randomUser = () => {
+  currentUser = users[Math.floor(Math.random() * users.length)];
+  return currentUser
+}
 
-window.addEventListener('load', function () {
-    displayRecipes();
-})
-
-allRecipesSection.addEventListener('click', clickRecipe);
-const checkboxes = document.querySelectorAll('input[type=checkbox][name=tag]')
+console.log(currentUser);
 
 
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', function () {
-        document.querySelectorAll('.checkbox').checked = true;
-        document.querySelectorAll('.checkbox').checked = false;
-
-        if (this.checked) {
-            tagList = Array.from(checkboxes)
-                .filter(i => i.checked)
-                .map(i => i.id)
-            showFilteredRecipes()
-
-              checkboxes.forEach(checkbox => {
-                if(!checkbox.checked){
-                  checkbox.disabled = true;
-                }
-              })
-
-        } else if (!this.checked) {
-          cookBook.filteredRecipes = []
-          allRecipesSection.innerHTML = ''
-          displayRecipes()
-          checkboxes.forEach(checkbox => {
-            checkbox.disabled = false
-          })
-        }
-        console.log(this)
-    });
-    
-});
 
 const showFilteredRecipes = () => {
-
     const filteredRecipes =  cookBook.filterByTags
-    
     (tagList).reduce((acc,recipe) => {
         allRecipesSection.innerHTML = ``
         acc += `
@@ -153,10 +128,52 @@ const showFilteredRecipes = () => {
         <h5>${recipe.name}</h5>
         </div> 
         `
-        
         return acc
     }, '');
-        console.log(tagList)
-        console.log(filteredRecipesSection)
         allRecipesSection.innerHTML = filteredRecipes
 }
+
+// event listeners
+
+window.addEventListener('load', function () {
+  filter = cookBook
+  displayRecipes();
+  randomUser();
+});
+
+
+// favoriteRecipesButton.addEventListener('click', function() {
+  
+// });
+
+
+allRecipesSection.addEventListener('click', clickRecipe);
+const checkboxes = document.querySelectorAll('input[type=checkbox][name=tag]')
+
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function () {
+    document.querySelectorAll('.checkbox').checked = true;
+    document.querySelectorAll('.checkbox').checked = false;
+
+    if (this.checked) {
+      tagList = Array.from(checkboxes)
+        .filter(i => i.checked)
+        .map(i => i.id)
+      showFilteredRecipes()
+
+      checkboxes.forEach(checkbox => {
+        if (!checkbox.checked) {
+          checkbox.disabled = true;
+        }
+      })
+    } else if (!this.checked) {
+      cookBook.filteredRecipes = []
+      allRecipesSection.innerHTML = ''
+      displayRecipes()
+      checkboxes.forEach(checkbox => {
+        checkbox.disabled = false
+      })
+    }
+  });
+
+});
