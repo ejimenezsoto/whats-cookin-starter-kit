72e772804abc23da63fae7445cd8da3bf65852d8
Jacob Quintana <jacobquintaa@gmail.com>
2021-12-11 19:01:00 -0700

    Create user class

--- src/classes/User.js
+++ src/classes/User.js
@@ -5,6 +5,7 @@ class User {
         this.pantry = pantry;
         this.favoriteRecipes = [];
         this.recipesToCook = [];
+        this.filteredFavoriteRecipes = [];
     }
 
     addToFavorites(recipe) {
@@ -17,9 +18,47 @@ class User {
     }
 
     addToCook(recipe) {
-        this.recipesToCook.push(recipe)
+        if (this.recipesToCook.includes(recipe)) {
+            const index = this.recipesToCook.indexOf(recipe)
+            this.recipesToCook.splice(index, 1)
+        } else {
+            this.recipesToCook.push(recipe)
+        }
+    }
+
+    filterByTags(tagList) {
+        const filteredTags = tagList.forEach(tag => {
+            const filteredRecipes = this.favoriteRecipes.forEach(recipe => {
+                if (recipe.tags.includes(tag)) {
+                    this.filteredFavoriteRecipes.push(recipe)
+                }
+            });
+        });
+
+        const filteredWithoutDuplicates = [... new Map(this.filteredFavoriteRecipes.map(recipe => [recipe.id, recipe])).values()]
+        return filteredWithoutDuplicates;
+    }
+    filterByKeyWord(userInput) {
+        const filterByName = this.favoriteRecipes.filter(recipe => recipe.name.toLowerCase().indexOf(userInput.toLowerCase()) !== -1);
+
+        const filtered = filterByName.slice()
+
+        const filterByIngredients = this.favoriteRecipes.forEach(recipe => {
+            const list = recipe.listIngredients().map(ingredient => {
+                return ingredient.toLowerCase()
+            })
+
+            const filterIngredientsName = list.forEach(ingredient => {
+                if (ingredient.indexOf(userInput.toLowerCase()) !== -1) {
+                    return filtered.push(recipe)
+                }
+            })
+
+        })
+
+        const filteredWithoutDuplicates = [... new Map(filtered.map(recipe => [recipe.id, recipe])).values()]
+
+        return filteredWithoutDuplicates
     }
 }
-
-
 export default User
