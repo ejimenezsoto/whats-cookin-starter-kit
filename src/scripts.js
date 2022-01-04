4f6f12bb5a5bab411108bc3bf6e2a920bac3253f
Jacob Quintana <jacobquintaa@gmail.com>
2022-01-03 17:36:45 -0700

    Create promise and add timout to window page load functions

--- src/scripts.js
+++ src/scripts.js
@@ -5,24 +5,25 @@ import './images/cooking.png'
 import './images/like.png'
 import './images/plus.png'
 import Recipe from './classes/Recipe';
-import recipeData from './data/recipes.js'
+// import recipeData from './data/recipes.js'
 import User from './classes/User';
-import usersData from './data/users';
+// import usersData from './data/users';
+import { userData, recipesData, ingredientData } from './apiCalls';
 
-// sections 
+// html sections 
 const allRecipesSection = document.querySelector('.all-recipes-section');
 const singleRecipeSection = document.querySelector('.single-recipe');
 const favoriteRecipeSection = document.querySelector('.my-favorites-section')
 const mealsToCookSection = document.querySelector('.meals-to-cook-section')
 
-//title
+// html titles
 const pageTitle = document.querySelector('.page-title');
 
-//inputs
+// html inputs
 const checkboxes = document.querySelectorAll('input[type=checkbox][name=tag]')
 const searchInput = document.querySelector('#userInput');
 
-//buttons
+// html buttons
 const favoriteRecipesButton = document.querySelector('#favoritesButton');
 const clearButton = document.querySelector('.clear-button');
 const allRecipesButton = document.querySelector('.recipies');
@@ -30,23 +31,42 @@ const yourGroceryListSectionButton = document.querySelector('.grocery-list');
 const mealsToCookButton = document.querySelector('.planner')
 
 
-//global variables *****************************
-const recipes = recipeData.map(recipe => {
-  return new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags)
-});
 
-const users = usersData.map(user => {
-  return new User(user.name, user.id, user.pantry)
-});
-
-const cookBook = new Cookbook(recipes);
+//global variables ***************************** 
 
 let tagList = [];
-let currentUser = users[Math.floor(Math.random() * users.length)];
 let currentRecipe;
+let recipes;
+let users;
+let cookBook;
+let filter;
+let currentUser;
 
-let filter = cookBook;
+// fetch apis *************************************
+Promise.all([userData, recipesData, ingredientData])
+.then(data => {
+  recipes = data[1].recipeData.map(recipe => {
+    return new Recipe(recipe.id, recipe.image, recipe.ingredients, recipe.instructions, recipe.name, recipe.tags, data[2].ingredientsData)
+  });
+  users = data[0].usersData.map(user => {
+    return new User(user.name, user.id, user.pantry)
+  });
 
+  console.log(data[2].ingredientsData);
+  cookBook = new Cookbook(recipes);
+  filter = cookBook;
+  currentUser = users[Math.floor(Math.random() * users.length)];
+})
+.catch(error => console.log('Oooops. Something is wrong', error))
+
+// const timeOut = () => { setTimeout(() => {
+//   console.log(recipes);
+//   console.log(users);
+//   console.log(cookBook);
+// }, 2000)
+// }
+
+// timeOut()
 //reusable functions ***************************
 const hide = (element) => {
 element.classList.add('hidden')
@@ -229,8 +249,11 @@ mealsToCookButton.addEventListener('click', showMealPlan);
 
     
 window.addEventListener('load', function () {
-  displayRecipes();
-  filterByCheckBoxes()
+  setTimeout(() => {
+    displayRecipes();
+    filterByCheckBoxes()
+    
+  }, 2000)
 });
 
 favoriteRecipesButton.addEventListener("click", () => {
