7a8e1f083954d33c955303897f6bdae46b0e3fe8
Jacob Quintana <jacobquintaa@gmail.com>
2022-01-05 19:41:33 -0700

    create pantry class and methods

--- src/classes/Pantry.js
+++ src/classes/Pantry.js
@@ -1,12 +1,15 @@
 
 class Pantry {
-  constructor(ingredients, ingredientsData, ) {
+  constructor(ingredients, ingredientsData) {
     this.ingredients = ingredients;
     this.ingredientsData = ingredientsData;
     this.listOfPantryIngredients = [];
   }
 
-  listIngredients() {
+  // we want to compare the ingredients from our users pantry to our 
+  //ingredients from each recipe that lives in the user.recipesToCook
+
+  listIngredientsNameAndAmount() {
     const ingredientIds = this.ingredients.map(ingredient => [ingredient.ingredient, ingredient.amount]);
 
     const ingredientDataObjs = ingredientIds.map(ingredientId => {
@@ -14,37 +17,41 @@ class Pantry {
     });
 
     const ingredientNames = ingredientDataObjs.map(ingredient => {
-      return this.listOfPantryIngredients.push(ingredient[0].name, ingredient[1])
+      return this.listOfPantryIngredients.push({name: ingredient[0].name, amount: ingredient[1]})
     });
-    return ingredientNames;
+
   }
 
 
-  checkPantry() {
-    const pantryArray = [].concat.apply([], this.listOfIngredients)
+  // checkPantry() {
+  //   const pantryArray = [].concat.apply([], this.listOfIngredients)
 
-    const checkRecipesToCook = this.recipesToCook.forEach(recipe => {
-      console.log('This is the recipe', recipe)
-      for (let i = 0; i < recipe.listOfRecipeIngredients.length; i += 2) {
-        if (!pantryArray.includes(recipe.listOfRecipeIngredients[i])) {
-          this.missingIngredients.push([recipe.listOfRecipeIngredients[i], recipe.listOfRecipeIngredients[i + 1]])
-          console.log('It hit')
-        }
+  //   const checkRecipesToCook = this.recipesToCook.forEach(recipe => {
+  //     console.log('This is the recipe', recipe)
+  //     for (let i = 0; i < recipe.listOfRecipeIngredients.length; i += 2) {
+  //       if (!pantryArray.includes(recipe.listOfRecipeIngredients[i])) {
+  //         this.missingIngredients.push([recipe.listOfRecipeIngredients[i], recipe.listOfRecipeIngredients[i + 1]])
+  //         console.log('It hit')
+  //       }
 
-        for (let j = 0; j < pantryArray.length; j += 2) {
-          if (pantryArray[j] === recipe.listOfRecipeIngredients[j] && pantryArray[j + 1] < recipe.listOfRecipeIngredients[i + 1]) {
-            this.missingIngredients.push([pantryArray[j], recipe.listOfRecipeIngredients[i + 1] - pantryArray[j + 1], 'I DONT HAVE ENOUGH'])
-          }
-        }
-      }
-    })
+  //       for (let j = 0; j < pantryArray.length; j += 2) {
+  //         if (pantryArray[j] === recipe.listOfRecipeIngredients[j] && pantryArray[j + 1] < recipe.listOfRecipeIngredients[i + 1]) {
+  //           this.missingIngredients.push([pantryArray[j], recipe.listOfRecipeIngredients[i + 1] - pantryArray[j + 1], 'I DONT HAVE ENOUGH'])
+  //         }
+  //       }
+  //     }
+  //   })
 
 
-    console.log(pantryArray, 'List of Pantry Ingredients')
-    console.log(this.recipesToCook[0].listOfRecipeIngredients, 'recipe List')
-    console.log(this.missingIngredients, 'Missing Ingredients')
+  //   console.log(pantryArray, 'List of Pantry Ingredients')
+  //   console.log(this.recipesToCook[0].listOfRecipeIngredients, 'recipe List')
+  //   console.log(this.missingIngredients, 'Missing Ingredients')
 
+  // }
+
+  checkPantry(currentRecipe) { 
   }
 }
+
 
 module.exports = Pantry
