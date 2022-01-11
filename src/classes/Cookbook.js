3d05da70ec08e8c03d5d2ab405387cdfb41f3705
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2022-01-10 19:19:05 -0800

    Html CSS refactoring

--- src/classes/Cookbook.js
+++ src/classes/Cookbook.js
@@ -1,39 +1,47 @@
-
 class Cookbook {
   constructor(recipes) {
     this.recipes = recipes;
     this.tags = [];
     this.filteredRecipes = [];
-  };
+  }
 
   filterByTags(tagList) {
-    const filteredTags = tagList.forEach(tag => {
-      const filteredRecipes = this.recipes.forEach(recipe => {
+    const filteredTags = tagList.forEach((tag) => {
+      const filteredRecipes = this.recipes.forEach((recipe) => {
         if (recipe.tags.includes(tag)) {
-          this.filteredRecipes.push(recipe)
+          this.filteredRecipes.push(recipe);
         }
       });
     });
-    const filteredWithoutDuplicates = [... new Map(this.filteredRecipes.map(recipe => [recipe.id, recipe])).values()];
+    const filteredWithoutDuplicates = [
+      ...new Map(
+        this.filteredRecipes.map((recipe) => [recipe.id, recipe])
+      ).values(),
+    ];
     return filteredWithoutDuplicates;
-  };
+  }
 
   filterByKeyWord(userInput) {
-    const filterByName = this.recipes.filter(recipe => recipe.name.toLowerCase().indexOf(userInput.toLowerCase()) !== -1);
+    const filterByName = this.recipes.filter(
+      (recipe) =>
+        recipe.name.toLowerCase().indexOf(userInput.toLowerCase()) !== -1
+    );
     const filtered = filterByName.slice();
-    const filterByIngredients = this.recipes.forEach(recipe => {
-      const list = recipe.listIngredientsNames().map(ingredient => {
-        return ingredient.toLowerCase()
+    const filterByIngredients = this.recipes.forEach((recipe) => {
+      const list = recipe.listIngredientsNames().map((ingredient) => {
+        return ingredient.toLowerCase();
       });
-      const filterIngredientsName = list.forEach(ingredient => {
+      const filterIngredientsName = list.forEach((ingredient) => {
         if (ingredient.indexOf(userInput.toLowerCase()) !== -1) {
           return filtered.push(recipe);
         }
       });
     });
-    const filteredWithoutDuplicates = [... new Map(filtered.map(recipe => [recipe.id, recipe])).values()];
+    const filteredWithoutDuplicates = [
+      ...new Map(filtered.map((recipe) => [recipe.id, recipe])).values(),
+    ];
     return filteredWithoutDuplicates;
-  };
-};
+  }
+}
 
 export default Cookbook;
